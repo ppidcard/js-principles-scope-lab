815430f17305e2080a3718ddc3c5be7b11452088
ppidcard <tvxqlikei@live.cn>
2021-04-17 23:42:18 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var  upperCaseCustomerName = function(){
+    return customerName = customerName.toUpperCase();
+}
+
+var setBestCustomer = function(){
+    bestCustomer = 'not bob';
+}
+
+var overwriteBestCustomer = function(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'hey';
+
+var changeLeastFavoriteCustomer = function(){
+    leastFavoriteCustomer = 'me';
+}
+
